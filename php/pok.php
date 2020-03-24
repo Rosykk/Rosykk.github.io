3d89fef5d98905648413e61aaf5578cf75bc2967
Rosykk <48493455+Rosykk@users.noreply.github.com>
2020-03-24 17:01:30 +0100

    test

---
+++ php/pok.php
@@ -0,0 +1,15 @@
+﻿<html>
+<body bgcolor="black" text="white" >
+<?php mail( "zdenek.mihu@seznam.cz",$p,$z) ?>
+<?php
+if ($p!="" and $z!="")
+{
+echo "<p><strong>Váš e-mail byl úspěšně odeslán</strong>.</p>";
+}
+else
+{
+echo "<p>Váš e-mail se <strong>nepodařilo odeslat.</strong> Pravděpodobně jste nevyplnili všechny údaje.</p>";
+} 
+?>
+</body>
+</html>
